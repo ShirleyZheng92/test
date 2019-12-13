17ca04884ed54cf999ce8116a974528fc02f5120
Xueli Zheng （郑雪莉） <52835750+ShirleyZheng92@users.noreply.github.com>
2019-12-13 11:22:38 -0800

    Add putData.py to put data in influxdb

---
+++ putData.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+from influxdb import InfluxDBClient
+import random
+import time
+random.seed()
+client = InfluxDBClient(database='mydb')
+
+while True:
+	json_body = [
+	    {
+	        "measurement": "temperature",
+	        "tags": {
+	            "machine": "unit42",
+	            "type": "assembly"
+	        },
+	        "fields": {
+	            "external": random.uniform(10, 30),
+	            "internal": random.uniform(25, 40),
+	        }
+	    },
+	    {
+	        "measurement": "temperature",
+	        "tags": {
+	            "machine": "unit21",
+	            "type": "assembly"
+	        },
+	        "fields": {
+	            "external": random.uniform(20, 35),
+	            "internal": random.uniform(40, 50),
+	        }
+	    }
+	]
+
+	client.write_points(json_body)
+	time.sleep(10)
